093b62faf08796a159a75aa83cd351e57728e525
Lewis Marshall <lewis@technoplusit.co.uk>
2017-02-14 17:36:54 +0000

    Try the actual hostname

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,6 @@ CNI_RELEASE=${CNI_RELEASE:-07a8a28637e97b22eb8dfe710eeae1344f69d16e}
 ARCH=${ARCH:-amd64}
 CNI_BIN_DIR=${CNI_BIN_DIR:-/opt/cni}
 IP=$(hostname -i)
-HOST=$(hostname)
 
 if [[ $1 == "coreos" ]]; then
 	BIN_DIR=${BIN_DIR:-/opt/bin}
@@ -90,7 +89,7 @@ if [[ ! -f ${ROOTFS}/etc/systemd/system/kubelet.service ]]; then
 	          --cluster-dns=10.96.0.10 \
 	          --cluster-domain=cluster.local \
 	          --node-ip=${IP} \
-	          --hostname-override=${IP}
+	          --hostname-override=${HOSTNAME}
 	Restart=always
 	StartLimitInterval=0
 	RestartSec=10
